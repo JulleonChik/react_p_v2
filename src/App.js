a1abb8662867a9894ab3abebc58d4b9fd47e4dd5
Julleon <julleonchik@gmail.com>
2024-11-11 21:12:14 +0300

    PizzaBlock Skeleton

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-
-import pizzasData from './assets/pizzas.json';
+import Skeleton from './components/PizzaBlock/Skeleton';
 
 function App() {
+  const [pizzasData, setPizzasData] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://67321e852a1b1a4ae10eee73.mockapi.io/items')
+      .then((res) => res.json())
+      .then((items) => {
+        setPizzasData(items);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -20,9 +31,11 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzasData.map((value) => {
-                return <PizzaBlock key={`pizza-block-${value.id}`} {...value} />;
-              })}
+              {isLoading
+                ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+                : pizzasData.map((value) => (
+                    <PizzaBlock key={`pizza-block-${value.id}`} {...value} />
+                  ))}
             </div>
           </div>
         </div>
